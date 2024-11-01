99f78cd43dca0abb0301d24eb2dc5b672a9ecfa7
CodeKnight314 <rtang24@nmhschool.org>
2024-11-01 11:45:21 +0000

    Correct model implementation

--- model.py
+++ model.py
@@ -1,17 +1,19 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import os
 
 class ConvBlock(nn.Module):
-    def __init__(self, input_channels, output_channels, stride):
+    def __init__(self, input_channels, output_channels, kernel_size=3, stride=1):
         super(ConvBlock, self).__init__()
+        padding = (kernel_size)//2
         self.depth_conv = nn.Sequential(
-            nn.Conv2d(input_channels, input_channels, kernel_size=3, stride=stride, padding=1, groups=input_channels),
+            nn.Conv2d(input_channels, input_channels, kernel_size=kernel_size, stride=stride, padding=padding, groups=input_channels, bias=False),
             nn.BatchNorm2d(input_channels),
             nn.ReLU()
         )
         self.point_conv = nn.Sequential(
-            nn.Conv2d(input_channels, output_channels, kernel_size=1, stride=1, padding=0),
+            nn.Conv2d(input_channels, output_channels, kernel_size=1, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(output_channels),
             nn.ReLU()
         )
@@ -26,22 +28,22 @@ class MobileNetEncoder(nn.Module):
         super(MobileNetEncoder, self).__init__()
 
         self.initial_conv = nn.Sequential(
-            nn.Conv2d(input_channels, 64, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(input_channels, 32, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU()
         )
 
         self.conv_stack = nn.ModuleList()
-        self.conv_channels = [64, 64, 128, 128, 256, 256, 512]
+        self.conv_channels = [32, 64, 128, 128, 256, 256, 512]
         for i in range(6):
             stride = 1 if i % 2 else 2
-            self.conv_stack.append(ConvBlock(self.conv_channels[i], self.conv_channels[i+1], stride))
+            self.conv_stack.append(ConvBlock(self.conv_channels[i], self.conv_channels[i+1], stride=stride))
 
         for i in range(5):
-            self.conv_stack.append(ConvBlock(512, 512, 1))
+            self.conv_stack.append(ConvBlock(512, 512, stride=1))
 
-        self.conv_stack.append(ConvBlock(512, 1024, 2))
-        self.conv_stack.append(ConvBlock(1024, 1024, 1))
+        self.conv_stack.append(ConvBlock(512, 1024, stride=2))
+        self.conv_stack.append(ConvBlock(1024, 1024, stride=1))
 
     def forward(self, x):
         features = []
@@ -55,46 +57,31 @@ class MobileNetEncoder(nn.Module):
 class DepthDecoder(nn.Module): 
     def __init__(self, input_channels: int):
         super().__init__()
-        self.upconv5 = nn.ConvTranspose2d(input_channels, 512, kernel_size=3, stride=2, padding=1, output_padding=1)
-        self.iconv5 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
         
-        self.upconv4 = nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, output_padding=1)
-        self.iconv4 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.iconv_1 = ConvBlock(input_channels=input_channels, output_channels=512, kernel_size=5, stride=1)
+        self.iconv_2 = ConvBlock(input_channels=512, output_channels=256, kernel_size=5, stride=1)
+        self.iconv_3 = ConvBlock(input_channels=512, output_channels=128, kernel_size=5, stride=1)
+        self.iconv_4 = ConvBlock(input_channels=256, output_channels=64, kernel_size=5, stride=1)
+        self.iconv_5 = ConvBlock(input_channels=96, output_channels=32, kernel_size=5, stride=1)
         
-        self.upconv3 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1)
-        self.iconv3 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-        
-        self.upconv2 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1)
-        self.iconv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        
-        self.upconv1 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1)
-        self.iconv1 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
-        
-        self.depth_predictor = nn.Conv2d(32, 1, kernel_size=1, stride=1, padding=0)
+        self.output = nn.Sequential(nn.Conv2d(in_channels=32, out_channels=1, kernel_size=1, stride=1, padding=0, bias=False),
+                                    nn.BatchNorm2d(1),
+                                    nn.ReLU(inplace=True))
     
     def forward(self, features):
-        x = features[-1]
-        x = self.upconv5(x)
+        x = self.iconv_1(features[-1])
+        x = F.interpolate(x, scale_factor=2, mode='nearest')
+        x = self.iconv_2(x)
+        x = F.interpolate(x, scale_factor=2, mode='nearest')
+        x = self.iconv_3(torch.cat([x, features[4]], dim=1))
+        x = F.interpolate(x, scale_factor=2, mode='nearest')
+        x = self.iconv_4(torch.cat([x, features[2]], dim=1))
+        x = F.interpolate(x, scale_factor=2, mode='nearest')
+        x = self.iconv_5(torch.cat([x, features[0]], dim=1))
+        x = F.interpolate(x, scale_factor=2, mode='nearest')
         
-        x = self.upconv4(x)
-        x = x + features[4]
-        x = self.iconv4(x)
-        
-        x = self.upconv3(x)
-        x = x + features[2]
-        x = self.iconv3(x)
-        
-        x = self.upconv2(x)
-        x = x + features[0]
-        x = self.iconv2(x)
-        
-        x = self.upconv1(x)
-        x = self.iconv1(x)
-        
-        depth = self.depth_predictor(x)
-        depth = torch.sigmoid(depth)
-
-        return depth
+        x = self.output(x)
+        return x
     
 class FastDepth(nn.Module): 
     def __init__(self, input_channels: int): 
@@ -102,6 +89,9 @@ class FastDepth(nn.Module):
         
         self.encoder = MobileNetEncoder(input_channels=input_channels)
         self.decoder = DepthDecoder(input_channels=1024)
+        
+        if os.path.exists("MobileNet_Imagenet100.pth"):
+            self.encoder.load_state_dict(torch.load("MobileNet_Imagenet100.pth", weights_only=True), strict=False)
         
     def forward(self, x): 
         x = self.encoder(x)
@@ -111,5 +101,5 @@ class FastDepth(nn.Module):
 if __name__ == "__main__":
     model = FastDepth(input_channels=3)
     input_tensor = torch.randn(1, 3, 224, 224)
-    depth_output = FastDepth(input_tensor)
+    depth_output = model(input_tensor)
     print("Output shape:", depth_output.shape)

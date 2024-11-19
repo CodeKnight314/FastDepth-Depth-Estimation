1ac26a5e6a46d1de2cec0a2a08aca2fba00cc5b4
CodeKnight314 <rtang24@nmhschool.org>
2024-11-19 23:41:40 +0000

    Update Depth Model

--- model.py
+++ model.py
@@ -1,42 +1,35 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision.models import resnet50, ResNet50_Weights
+from torchvision.models import resnet18, ResNet18_Weights
 
 class ResNetDepth(nn.Module):
     def __init__(self):
         super().__init__()
 
-        resnet = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
+        resnet = resnet18(weights=ResNet18_Weights.DEFAULT)
         self.encoder_layers = list(resnet.children())[:-2]
         self.encoder = nn.Sequential(*self.encoder_layers)
 
-        self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(2048, 1024, kernel_size=3, stride=2, padding=1, output_padding=1),
-            nn.BatchNorm2d(1024),
-            nn.ReLU(inplace=True),
+        self.upconv4 = nn.Conv2d(512, 256, kernel_size=3, padding=1)
+        self.upconv3 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
+        self.upconv2 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
+        self.upconv1 = nn.Conv2d(64, 32, kernel_size=3, padding=1)
 
-            nn.ConvTranspose2d(1024, 512, kernel_size=3, stride=2, padding=1, output_padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(inplace=True),
+        self.upsample = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
 
-            nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, output_padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-
-            nn.ConvTranspose2d(256, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-
-            nn.Conv2d(64, 1, kernel_size=1), 
-            nn.Sigmoid()
-        )
+        self.output_layer = nn.Conv2d(32, 1, kernel_size=3, padding=1)
 
     def forward(self, x):
         x = self.encoder(x)
-        x = self.decoder(x)
-        return x
+        x = self.upsample(F.relu(self.upconv4(x)))
+        x = self.upsample(F.relu(self.upconv3(x)))
+        x = self.upsample(F.relu(self.upconv2(x)))
+        x = self.upsample(F.relu(self.upconv1(x)))
 
+        x = self.upsample(F.relu(self.output_layer(x)))
+
+        return torch.sigmoid(x)
 
 if __name__ == "__main__":
     model = ResNetDepth(input_channels=3)

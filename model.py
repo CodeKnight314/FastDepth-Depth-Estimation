551f6cb3732e42ee30b701493968ade71963b7ee
CodeKnight314 <tang.ri@northeastern.edu>
2024-12-15 21:47:49 +0000

    Rework model again :,)

--- model.py
+++ model.py
@@ -5,248 +5,158 @@ import torchvision.models as models
 import math
 from torchvision.models import ResNet18_Weights, ResNet34_Weights
 
-def weights_init(m):
-    if isinstance(m, nn.Conv2d):
-        n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-        m.weight.data.normal_(0, math.sqrt(2. / n))
-        if m.bias is not None:
-            m.bias.data.zero_()
-    elif isinstance(m, nn.ConvTranspose2d):
-        n = m.kernel_size[0] * m.kernel_size[1] * m.in_channels
-        m.weight.data.normal_(0, math.sqrt(2. / n))
-        if m.bias is not None:
-            m.bias.data.zero_()
-    elif isinstance(m, nn.BatchNorm2d):
-        m.weight.data.fill_(1)
-        m.bias.data.zero_()
+class ResNetEncoder(nn.Module):
+    """
+    ResNet encoder that extracts features at multiple scales for the decoder.
+    Configurable to ResNet-18 or ResNet-34.
+    """
+    def __init__(self, resnet_type='resnet18', input_channels=3):
+        super(ResNetEncoder, self).__init__()
+        assert resnet_type in ['resnet18', 'resnet34'], "resnet_type must be 'resnet18' or 'resnet34'."
 
-class ConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size):
-        super().__init__()
-        padding = (kernel_size-1)//2
+        # Load pretrained model
+        if resnet_type == 'resnet18':
+            self.resnet = models.resnet18(weights=ResNet18_Weights.DEFAULT)
+        else:
+            self.resnet = models.resnet34(weight=ResNet34_Weights.DEFAULT)
 
-        self.conv_block = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=padding, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU()
-        )
+        # Adjust first layer if input_channels != 3
+        if input_channels != 3:
+            old_weights = self.resnet.conv1.weight.data
+            if input_channels == 1:
+                # average across input channels
+                new_weights = old_weights.mean(dim=1, keepdim=True)
+            else:
+                raise ValueError("Only 1 or 3 input channels are supported.")
 
-    def forward(self, x):
-        return self.conv_block(x)
+            self.resnet.conv1 = nn.Conv2d(input_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+            self.resnet.conv1.weight.data = new_weights
 
-class MConvBlock(nn.Module):
-    def __init__(self, input_channels, output_channels, kernel_size=3, stride=1):
-        super(MConvBlock, self).__init__()
-
-        padding = (kernel_size-1)//2
-        self.depth_conv = nn.Sequential(
-            nn.Conv2d(input_channels, input_channels, kernel_size=kernel_size, stride=stride, padding=padding, groups=input_channels),
-            nn.BatchNorm2d(input_channels),
-            nn.ReLU()
-        )
-
-        self.point_conv = nn.Sequential(
-            nn.Conv2d(input_channels, output_channels, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(output_channels),
-            nn.ReLU()
-        )
+        # Keep references to layers we need
+        self.layer1 = self.resnet.layer1
+        self.layer2 = self.resnet.layer2
+        self.layer3 = self.resnet.layer3
+        self.layer4 = self.resnet.layer4
+        self.bn1 = self.resnet.bn1
+        self.relu = self.resnet.relu
+        self.conv1 = self.resnet.conv1
+        self.maxpool = self.resnet.maxpool
 
     def forward(self, x):
-        x = self.depth_conv(x)
-        x = self.point_conv(x)
+        # Instead of including maxpool in x0, we only go up to relu:
+        # Input: B,C,H,W
+
+        # x0 after initial conv, bn, relu (no maxpool) -> Scale: H/2, W/2
+        x0 = self.conv1(x)
+        x0 = self.bn1(x0)
+        x0 = self.relu(x0)  # (B,64,H/2,W/2)
+
+        # Now apply maxpool and layer1 to get x1 -> Scale: H/4, W/4
+        x1 = self.maxpool(x0)
+        x1 = self.layer1(x1)   # (B,64,H/4,W/4)
+
+        # Further layers
+        x2 = self.layer2(x1)   # (B,128,H/8,W/8)
+        x3 = self.layer3(x2)   # (B,256,H/16,W/16)
+        x4 = self.layer4(x3)   # (B,512,H/32,W/32)
+
+        return x0, x1, x2, x3, x4
+
+
+class UpConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, upsample=True):
+        super(UpConvBlock, self).__init__()
+        self.upsample = upsample
+        if self.upsample:
+            self.up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+        else:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+        self.conv1 = nn.Conv2d(out_channels, out_channels, kernel_size, stride, padding, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu1 = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.up(x)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self, enc_channels=[64, 64, 128, 256, 512], out_channels=1):
+        super(Decoder, self).__init__()
+
+        # x4:1/32 -> x3:1/16
+        self.up4 = UpConvBlock(in_channels=enc_channels[4], out_channels=enc_channels[3])
+        self.iconv4 = nn.Sequential(
+            nn.Conv2d(enc_channels[3]*2, enc_channels[3], kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(enc_channels[3]),
+            nn.ReLU(inplace=True)
+        )
+
+        # x3:1/16 -> x2:1/8
+        self.up3 = UpConvBlock(in_channels=enc_channels[3], out_channels=enc_channels[2])
+        self.iconv3 = nn.Sequential(
+            nn.Conv2d(enc_channels[2]*2, enc_channels[2], kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(enc_channels[2]),
+            nn.ReLU(inplace=True)
+        )
+
+        # x2:1/8 -> x1:1/4
+        self.up2 = UpConvBlock(in_channels=enc_channels[2], out_channels=enc_channels[1])
+        self.iconv2 = nn.Sequential(
+            nn.Conv2d(enc_channels[1]*2, enc_channels[1], kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(enc_channels[1]),
+            nn.ReLU(inplace=True)
+        )
+
+        # x1:1/4 -> x0:1/2
+        self.up1 = UpConvBlock(in_channels=enc_channels[1], out_channels=enc_channels[0])
+        self.iconv1 = nn.Sequential(
+            nn.Conv2d(enc_channels[0]*2, enc_channels[0], kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(enc_channels[0]),
+            nn.ReLU(inplace=True)
+        )
+
+        # x0:1/2 -> full resolution:1/1
+        self.up0 = UpConvBlock(in_channels=enc_channels[0], out_channels=enc_channels[0]//2 if enc_channels[0]>1 else 1)
+        self.iconv0 = nn.Sequential(
+            nn.Conv2d((enc_channels[0]//2 if enc_channels[0]>1 else 1), out_channels, kernel_size=3, padding=1, bias=False)
+        )
+
+    def forward(self, x0, x1, x2, x3, x4):
+        x = self.up4(x4)  # 1/32 -> 1/16
+        x = torch.cat((x, x3), dim=1)
+        x = self.iconv4(x)
+
+        x = self.up3(x)   # 1/16 -> 1/8
+        x = torch.cat((x, x2), dim=1)
+        x = self.iconv3(x)
+
+        x = self.up2(x)   # 1/8 -> 1/4
+        x = torch.cat((x, x1), dim=1)
+        x = self.iconv2(x)
+
+        x = self.up1(x)   # 1/4 -> 1/2
+        x = torch.cat((x, x0), dim=1)
+        x = self.iconv1(x)
+
+        x = self.up0(x)   # 1/2 -> 1/1
+        x = self.iconv0(x)
+
         return x
 
 class ResNetFastDepth(nn.Module):
-    def __init__(self, layers: int, output_size: int, in_channels: int = 3, pretrained: bool = True, mode: str = "skip"):
-        super().__init__()
-
-        if layers not in [18, 34]:
-            raise ValueError(f"Only 18 and 34 layers model are defined for ResNet. Got {layers}")
-
-        self.output_size = output_size
-        if pretrained:
-            if layers == 18:
-                weights = ResNet18_Weights.IMAGENET1K_V1
-            elif layers == 34:
-                weights = ResNet34_Weights.IMAGENET1K_V1
-        else:
-            weights = None
-
-        model = models.__dict__[f'resnet{layers}'](weights=weights)
-        if not pretrained:
-            model.apply(weights_init)
-
-        if in_channels == 3:
-            self.conv1 = model._modules['conv1']
-            self.bn1 = model._modules['bn1']
-        else:
-            self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
-            self.bn1 = nn.BatchNorm2d(64)
-            weights_init(self.conv1)
-            weights_init(self.bn1)
-
-        self.relu = model._modules['relu']
-        self.maxpool = model._modules['maxpool']
-        self.layer1 = model._modules['layer1']
-        self.layer2 = model._modules['layer2']
-        self.layer3 = model._modules['layer3']
-        self.layer4 = model._modules['layer4']
-
-        if layers <= 34:
-            num_channels = 512
-        elif layers == 50:
-            num_channels = 2048
-            mode = None
-        self.conv2 = nn.Conv2d(in_channels=num_channels, out_channels=1024, kernel_size=1, stride=1, padding=0)
-        weights_init(self.conv2)
-
-        kernel_size = 5
-        if mode == "skip":
-            self.decode_conv1 = ConvBlock(1024, 512, kernel_size)
-            self.decode_conv2 = ConvBlock(512, 256, kernel_size)
-            self.decode_conv3 = ConvBlock(256, 128, kernel_size)
-            self.decode_conv4 = ConvBlock(128, 64, kernel_size)
-            self.decode_conv5 = ConvBlock(64, 32, kernel_size)
-            self.decode_conv6 = nn.Conv2d(in_channels=32, out_channels=1, kernel_size=1, stride=1, padding=0)
-        elif mode == "concat":
-            self.decode_conv1 = ConvBlock(1024, 512, kernel_size)
-            self.decode_conv2 = ConvBlock(768, 256, kernel_size)
-            self.decode_conv3 = ConvBlock(384, 128, kernel_size)
-            self.decode_conv4 = ConvBlock(192, 64, kernel_size)
-            self.decode_conv5 = ConvBlock(128, 32, kernel_size)
-            self.decode_conv6 = nn.Conv2d(in_channels=32, out_channels=1, kernel_size=1, stride=1, padding=0)
-        else:
-            self.decode_conv1 = ConvBlock(1024, 512, kernel_size)
-            self.decode_conv2 = ConvBlock(512, 256, kernel_size)
-            self.decode_conv3 = ConvBlock(256, 128, kernel_size)
-            self.decode_conv4 = ConvBlock(128, 64, kernel_size)
-            self.decode_conv5 = ConvBlock(64, 32, kernel_size)
-            self.decode_conv6 = nn.Conv2d(in_channels=32, out_channels=1, kernel_size=1, stride=1, padding=0)
-
-        weights_init(self.decode_conv1)
-        weights_init(self.decode_conv2)
-        weights_init(self.decode_conv3)
-        weights_init(self.decode_conv4)
-        weights_init(self.decode_conv5)
-        weights_init(self.decode_conv6)
-
-        self.mode = mode
+    def __init__(self, resnet_type='resnet18', pretrained=True, input_channels=3):
+        super(ResNetFastDepth, self).__init__()
+        self.encoder = ResNetEncoder(resnet_type=resnet_type, pretrained=pretrained, input_channels=input_channels)
+        enc_channels = [64, 64, 128, 256, 512]
+        self.decoder = Decoder(enc_channels=enc_channels, out_channels=1)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x1 = self.relu(x)
-        x2 = self.maxpool(x1)
-        x3 = self.layer1(x2)
-        x4 = self.layer2(x3)
-        x5 = self.layer3(x4)
-        x6 = self.layer4(x5)
-
-        x7 = self.conv2(x6)
-
-        if self.mode == "skip":
-            y10 = self.decode_conv1(x7)
-            y9 = F.interpolate(y10 + x6, scale_factor=2, mode='nearest')
-            y8 = self.decode_conv2(y9)
-            y7 = F.interpolate(y8 + x5, scale_factor=2, mode='nearest')
-            y6 = self.decode_conv3(y7)
-            y5 = F.interpolate(y6 + x4, scale_factor=2, mode='nearest')
-            y4 = self.decode_conv4(y5)
-            y3 = F.interpolate(y4 + x3, scale_factor=2, mode='nearest')
-            y2 = self.decode_conv5(y3 + x1)
-            y1 = F.interpolate(y2, scale_factor=2, mode='nearest')
-            y = self.decode_conv6(y1)
-        elif self.mode == "concat":
-            y10 = self.decode_conv1(x7)
-            y9 = F.interpolate(y10, scale_factor=2, mode='nearest')
-            y8 = self.decode_conv2(torch.cat((y9, x5), 1))
-            y7 = F.interpolate(y8, scale_factor=2, mode='nearest')
-            y6 = self.decode_conv3(torch.cat((y7, x4), 1))
-            y5 = F.interpolate(y6, scale_factor=2, mode='nearest')
-            y4 = self.decode_conv4(torch.cat((y5, x3), 1))
-            y3 = F.interpolate(y4, scale_factor=2, mode='nearest')
-            y2 = self.decode_conv5(torch.cat((y3, x1), 1))
-            y1 = F.interpolate(y2, scale_factor=2, mode='nearest')
-            y = self.decode_conv6(y1)
-        else:
-            y10 = self.decode_conv1(x7)
-            y9 = F.interpolate(y10, scale_factor=2, mode='nearest')
-            y8 = self.decode_conv2(y9)
-            y7 = F.interpolate(y8, scale_factor=2, mode='nearest')
-            y6 = self.decode_conv3(y7)
-            y5 = F.interpolate(y6, scale_factor=2, mode='nearest')
-            y4 = self.decode_conv4(y5)
-            y3 = F.interpolate(y4, scale_factor=2, mode='nearest')
-            y2 = self.decode_conv5(y3)
-            y1 = F.interpolate(y2, scale_factor=2, mode='nearest')
-            y = self.decode_conv6(y1)
-
-        return y
-
-class MobileNetFastDepth(nn.Module): 
-    def __init__(self, input_channels: int = 3): 
-        super().__init__() 
-        
-        self.initial_conv = nn.Sequential(
-            nn.Conv2d(input_channels, 32, kernel_size=3, stride=2, padding=1), 
-            nn.BatchNorm2d(32), 
-            nn.ReLU()
-        )
-        
-        self.conv_stack = nn.ModuleList()
-        self.conv_channels = [32, 64, 128, 128, 256, 256, 512]
-        for i in range(6): 
-            stride = 1 if i % 2 else 2
-            self.conv_stack.append(MConvBlock(input_channels=self.conv_channels[i], output_channels=self.conv_channels[i+1], stride=stride))
-        
-        for i in range(5): 
-            self.conv_stack.append(MConvBlock(512, 512, stride=1))
-        
-        self.conv_stack.append(MConvBlock(512, 1024, stride=2))
-        self.conv_stack.append(MConvBlock(1024, 1024, 1))
-
-        kernel_size = 5
-
-        self.decode_conv1 = MConvBlock(input_channels=1024, output_channels=512, kernel_size=kernel_size)
-        self.decode_conv2 = MConvBlock(input_channels=1024, output_channels=256, kernel_size=kernel_size)
-        self.decode_conv3 = MConvBlock(input_channels=512, output_channels=128, kernel_size=kernel_size)  # Concatenate with x3
-        self.decode_conv4 = MConvBlock(input_channels=256, output_channels=64, kernel_size=kernel_size)   # Concatenate with x2
-        self.decode_conv5 = MConvBlock(input_channels=64, output_channels=32, kernel_size=kernel_size)    # Concatenate with x1
-        self.decode_conv6 = nn.Conv2d(in_channels=32, out_channels=1, kernel_size=1, stride=1, padding=0)
-        
-        weights_init(self.decode_conv1)
-        weights_init(self.decode_conv2)
-        weights_init(self.decode_conv3)
-        weights_init(self.decode_conv4)
-        weights_init(self.decode_conv5)
-        weights_init(self.decode_conv6)
-
-    def forward(self, x):
-        x = self.initial_conv(x)
-        for i, layer in enumerate(self.conv_stack):
-            x = layer(x)
-            print(i, x.shape)
-            if i == 5: 
-                x1 = x  # Save intermediate feature map
-            elif i == 3: 
-                x2 = x  # Save intermediate feature map
-            elif i == 1: 
-                x3 = x  # Save intermediate feature map
-
-        x = self.decode_conv1(x)
-        x = F.interpolate(x, scale_factor=2, mode='nearest')
-
-        x = self.decode_conv2(torch.cat([x1, x], dim=1))
-        x = F.interpolate(x, scale_factor=2, mode='nearest')
-
-        x = self.decode_conv3(torch.cat([x2, x], dim=1))  # Concatenate with x3
-        x = F.interpolate(x, scale_factor=2, mode='nearest')
-
-        x = self.decode_conv4(torch.cat([x3, x], dim=1))  # Concatenate with x2
-        x = F.interpolate(x, scale_factor=2, mode='nearest')
-
-        x = self.decode_conv5(x)  # Concatenate with x1
-        x = F.interpolate(x, scale_factor=2, mode='nearest')
-
-        x = self.decode_conv6(x)
-        return x
+        x0, x1, x2, x3, x4 = self.encoder(x)
+        depth = self.decoder(x0, x1, x2, x3, x4)
+        depth = torch.sigmoid(depth)
+        return depth
